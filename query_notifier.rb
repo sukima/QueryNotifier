fe7e8aef70c50832f8e9fc0438e11ade97cde2d0
Devin Weaver <suki@tritarget.org>
2011-03-11 16:03:20 -0500

    Adds initial skelleton

--- query_notifier.rb
+++ query_notifier.rb
@@ -5,7 +5,4 @@ require 'bundler/setup'
 
 require 'sinatra'
 
-get '/' do
-  "Hello world, it's #{Time.now} at the server!"
-end
 # vim:set et sw=2 ts=8:
--- test/query_notifier_test.rb
+++ test/query_notifier_test.rb
@@ -8,6 +8,5 @@ class QueryNotifierTest < Test::Unit::TestCase
   def app
     Sinatra::Application
   end
-
 end
 # vim:set et sw=2 ts=8:
